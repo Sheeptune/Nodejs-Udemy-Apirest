372cd3ec13ba1bbc81ad94647f8cfb675473e3b8
Sheeptune <gwenlhd@gmail.com>
2019-04-08 13:10:56 +0200

    Url : creation and parameters setting

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const app = express()
 const morgan = require('morgan')
 app.use(morgan('dev'))
 
+// Creation of url
+app.get('/api', (res, req) =>{
+    res.setEncoding('Root API')
+})
+
+app.get('/api/v1' (res, req) =>{
+    res.send('API Version 1')
+})
 
 
-
+// Setting url parameter with req.params
+app.get('api/v1/books:id', (req, res)=>{
+    res.send(req.params)
+})
 
 app.listen(8080, ()=>{
     console.log('Started on port 8080')
