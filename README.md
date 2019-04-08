e28bcc5bf3c6e11b9a550ec032ec37fae2b69cc4
Sheeptune <gwenlhd@gmail.com>
2019-04-08 15:12:36 +0200

    Creating success and error functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 NodeJs Udemy API REST
+
+This project is just a training based on a Udemy Tutorial to take my first steps with Node Js.
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Appealing to Success and Error functions
+const func = require('functions')
 // Appealing to Express throught "app"
 const express = require('express')
 const app = express()
@@ -23,18 +25,23 @@ const members = [
 app.use(morgan('dev'))
 
 // Url creation and parameters setting
-app.get('/api/v1/members/:id' ,(req, res) => {
-    res.send(members[(req.params.id)-1].name)
+// By convention we replaced all res.send by res.json to stringify automatically
+// and we set both success and error functions (we can find in the node_modules folder)
+app.get('/api/v1/members/:id', (req, res) => {
+    res.json(success(members[(req.params.id)-1].name))
 })
 
 app.get('/api/v1/members', (req, res) => {
     // Using req.query ?=max to display the array
     if (req.query.max != undefined && req.query.max > 0) {
-        res.send(members.slice(0, req.quuery.max))
+        res.json(success(members.slice(0, req.quuery.max)))
+    } else if (req.query.max != undefined) {
+        res.json(error('Wrong max value'))
     } else {
-    res.send(members)
+        res.json(success(members))
+        }
     }
-})
+)
 
 // Listenning 
 app.listen(8080, ()=>{
