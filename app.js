90c79f129cd17679fda37a646ba832645e9b98c6
Sheeptune <gwenlhd@gmail.com>
2019-04-09 10:38:52 +0200

    Adding members with POST and BodyParser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Appealing to Success and Error functions
 const func = require('functions')
+// Appealing to body-parser middleware
+const bodyParser = require('body-parser')
 // Appealing to Express throught "app"
 const express = require('express')
 const app = express()
@@ -24,6 +26,10 @@ const members = [
 // Appealing to Morgan to get logging request details
 app.use(morgan('dev'))
 
+// for parsing application/json => for more information scroll to the POST method
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 // Url creation and parameters setting
 // By convention we replaced all res.send by res.json to stringify automatically
 // and we set both success and error functions (we can find in the node_modules folder)
@@ -39,9 +45,29 @@ app.get('/api/v1/members', (req, res) => {
         res.json(func.error('Wrong max value'))
     } else {
         res.json(func.success(members))
-        }
     }
-)
+})
+
+// Using POST to add new members to the array
+// We need to use BodyParser in order to parse the data into the body
+app.post('/api/v1/members', (req, res) => {
+    // We check if the parameters does exist to add a new member
+    if (req.body.name) {
+
+        let member = {
+            id: members.length+1,
+            name: req.body.name
+        }
+
+        members.push(member)
+
+        res.json(func.success(member))
+
+    } else {
+        res.json(func.error('no name value'))
+    }
+
+})
 
 // Listenning 
 app.listen(8080, ()=>{
