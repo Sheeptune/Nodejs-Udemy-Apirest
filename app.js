c89eb5c968a42f6ee5d75ef7aec81345336a1dfa
Sheeptune <gwenlhd@gmail.com>
2019-04-10 10:12:47 +0200

    Creating routes with Express

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ const members = [
     }
 ]
 
+// ROUTER ** > Creation of a router for the branch "members"
+// The router is linked to Express
+let MembersRouter = express.Router()
+
 // Appealing to Morgan to get logging request details
 app.use(morgan('dev'))
 
@@ -32,110 +36,119 @@ app.use(morgan('dev'))
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-// Url creation and parameters setting
-// By convention we replaced all res.send by res.json to stringify automatically
-// and we set both success and error functions (we can find in the node_modules folder)
-app.get('/api/v1/members/:id', (req, res) => {
+// ROUTER **
+MembersRouter.route('/:id')
 
-    let index = getIndex(req.params.id);
+    // Url creation and parameters setting
+    // By convention we replaced all res.send by res.json to stringify automatically
+    // and we set both success and error functions (we can find in the node_modules folder)
+    // .get is automatically considering MembersRouter !!!
+    .get((req, res) => {
 
-    // We will get the members by using the getIndex function
-    if (typeof(index) == 'string') {
-        res.json(error(index))
-    } else {
+        let index = getIndex(req.params.id);
 
-        res.json(success(members[index]))
-    }
-})
-
-//Using PUT to modify members
-app.put('/api/v1/members/:id', (req, res) =>{
-
-    let index = getIndex(req.params.id);
-
-    if (typeof(index) == 'string') {
-        res.json(error(index))
-    } else {
-        // If not a string, save mofication
-        //let member = members[index]
-        let same = false
-        
-        // Loop to make sure that the name is not already taken
-        for (let i = 0; i < members.length; i++){
-            // if the name is already taken & if the id is not the same that is used
-            if (req.body.name == members[i].name && req.params.id != members[i].id) {
-                same = true
-                break
-            }     
-        } // if same Name true error, if not success
-            if (same){
-                res.json(error('same name'))
-            } else {
-                members[index] = req.body.name
-                res.json(success(true))
-        }
-    }
-})
-
-// Using DELETE to delete members
-app.delete('/api/v1/members/:id', (req, res)=>{
-
-    let index = getIndex(req.params.id);
-
-    // does the id already exist
-
-    if (typeof(index) == 'string') {
-        res.json(error(index))
-    } else {
-        members.splice(index, 1)
-        res.json(success(members))
-    }
-})
-
-
-
-app.get('/api/v1/members', (req, res) => {
-    // Using req.query ?=max to display the array
-    if (req.query.max != undefined && req.query.max > 0) {
-        res.json(success(members.slice(0, req.quuery.max)))
-    } else if (req.query.max != undefined) {
-        res.json(error('Wrong max value'))
-    } else {
-        res.json(success(members))
-    }
-})
-
-// Using POST to add new members to the array
-// We need to use BodyParser in order to parse the data into the body
-app.post('/api/v1/members', (req, res) => {
-    // We check if the parameters does exist to add a new member
-    if (req.body.name) {
-
-        let sameName = false
-
-        // Check if the name is available or not
-        for (let i = 0; i < members.length; i++){
-            if (members[i].name == req.body.name) {
-                sameName = true
-                break
-            }
-        }
-
-        // If the name is already taken
-        if (sameName) {
-            res.json(error('This name is already taken'))
+        // We will get the members by using the getIndex function
+        if (typeof(index) == 'string') {
+            res.json(error(index))
         } else {
 
-            let member = {
-            id: createID(),
-            name: req.body.name
+            res.json(success(members[index]))
+        }
+    })
+
+    //Using PUT to modify members
+    .put((req, res) =>{
+
+        let index = getIndex(req.params.id);
+
+        if (typeof(index) == 'string') {
+            res.json(error(index))
+        } else {
+            // If not a string, save mofication
+            //let member = members[index]
+            let same = false
+            
+            // Loop to make sure that the name is not already taken
+            for (let i = 0; i < members.length; i++){
+                // if the name is already taken & if the id is not the same that is used
+                if (req.body.name == members[i].name && req.params.id != members[i].id) {
+                    same = true
+                    break
+                }     
+            } // if same Name true error, if not success
+                if (same){
+                    res.json(error('same name'))
+                } else {
+                    members[index] = req.body.name
+                    res.json(success(true))
+            }
+        }
+    })
+
+    // Using DELETE to delete members and their id
+    .delete((req, res)=>{
+
+        let index = getIndex(req.params.id);
+
+        // does the id already exist
+
+        if (typeof(index) == 'string') {
+            res.json(error(index))
+        } else {
+            members.splice(index, 1)
+            res.json(success(members))
+        }
+    })
+
+// ROUTER **
+MembersRouter.route('/')
+
+    // Get all the members and their id
+    .get((req, res) => {
+        // Using req.query ?=max to display the array
+        if (req.query.max != undefined && req.query.max > 0) {
+            res.json(success(members.slice(0, req.quuery.max)))
+        } else if (req.query.max != undefined) {
+            res.json(error('Wrong max value'))
+        } else {
+            res.json(success(members))
+        }
+    })
+
+    // Using POST to add new members to the array
+    // We need to use BodyParser in order to parse the data into the body
+    .post((req, res) => {
+        // We check if the parameters does exist to add a new member
+        if (req.body.name) {
+
+            let sameName = false
+
+            // Check if the name is available or not
+            for (let i = 0; i < members.length; i++){
+                if (members[i].name == req.body.name) {
+                    sameName = true
+                    break
+                }
             }
 
-            members.push(member)
-            res.json(success(member))
+            // If the name is already taken
+            if (sameName) {
+                res.json(error('This name is already taken'))
+            } else {
+
+                let member = {
+                id: createID(),
+                name: req.body.name
+                }
+
+                members.push(member)
+                res.json(success(member))
+            }
         }
-    }
-})
+    })
+
+// ROUTER ** > MembersRouter is taking action over '/api/v1/members'
+app.use('/api/v1/members', MembersRouter)
 
 // Listenning 
 app.listen(8080, ()=>{
